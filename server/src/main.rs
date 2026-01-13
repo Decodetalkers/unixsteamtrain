1d263d3c9443c41926374869eff212ae7202d058
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-01-13 23:12:38 +0900

    chore: update

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,29 @@
+use std::os::unix::net::{UnixListener, UnixStream};
+use std::thread;
+use stream_message::{Request, Response, SyncCodec};
+
+fn handle_client(mut stream: UnixStream) {
+    loop {
+        let Ok(Request::Remote { monitors }) = Request::read_from(&mut stream) else {
+            continue;
+        };
+        println!("monitors: {monitors:?}");
+        let _ = Response::Success { index: 1 }.write_to(&mut stream);
+    }
+}
+
 fn main() {
+    let listener = UnixListener::bind("./server.sock").unwrap();
+    let mut threads = vec![];
+    for stream in listener.incoming() {
+        let Ok(stream) = stream else {
+            break;
+        };
+        let handler = thread::spawn(|| handle_client(stream));
+        threads.push(handler);
+    }
+    for thread in threads {
+        let _ = thread.join();
+    }
     println!("Hello, world!");
 }
---
+++ stream_message/src/lib.rs
@@ -0,0 +1,61 @@
+pub mod error;
+use std::io::{Read, Write};
+
+use error::Error;
+
+use serde::{Deserialize, Serialize};
+
+pub trait SyncCodec {
+    fn read_from<T: Read>(stream: &mut T) -> Result<Self, Error>
+    where
+        Self: std::marker::Sized;
+    fn write_to<T: Write>(&self, stream: &mut T) -> Result<(), Error>;
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+#[serde(tag = "type")]
+pub enum Request {
+    Remote { monitors: Vec<String> },
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+#[serde(tag = "type")]
+pub enum Response {
+    Success { index: u32 },
+    Cancel,
+    Busy,
+}
+
+macro_rules! impl_message {
+    ($message:ident) => {
+        impl SyncCodec for $message {
+            fn read_from<T: Read>(stream: &mut T) -> Result<Self, Error> {
+                let mut len_bytes = [0; 4];
+                stream
+                    .read_exact(&mut len_bytes)
+                    .map_err(|e| match e.kind() {
+                        std::io::ErrorKind::UnexpectedEof => Error::Eof,
+                        _ => e.into(),
+                    })?;
+                let len = u32::from_ne_bytes(len_bytes);
+
+                let mut resp_buf = vec![0; len as usize];
+                stream.read_exact(&mut resp_buf)?;
+                serde_json::from_slice(&resp_buf).map_err(|e| e.into())
+            }
+
+            fn write_to<T: Write>(&self, stream: &mut T) -> Result<(), Error> {
+                let body_bytes = serde_json::to_vec(self)?;
+                let len_bytes = (body_bytes.len() as u32).to_ne_bytes();
+                stream.write_all(&len_bytes)?;
+                stream.write_all(&body_bytes)?;
+                Ok(())
+            }
+        }
+    };
+}
+
+impl_message!(Response);
+impl_message!(Request);
